3995026acbfa6d26f5853ab3f41c34c09ebe419c
Lalit-N97 <lalitnegi1997@gmail.com>
2022-05-14 23:58:26 +0530

    add react routes

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,10 +1,26 @@
-import './App.css';
-import { TeamPage } from './pages/TeamPage'
+import './App.scss';
+import {HashRouter as Router, Route, Switch} from 'react-router-dom';
+import { TeamPage } from './pages/TeamPage';
+import { MatchPage } from './pages/MatchPage';
+import { HomePage } from './pages/HomePage';
 
 function App() {
   return (
     <div className="App">
-      <TeamPage/>
+      <Router>
+        <Switch>
+          <Route path="/teams/:teamName/matches/:year">
+            <MatchPage />
+          </Route>
+          <Route path="/teams/:teamName">
+            <TeamPage />
+          </Route>
+          <Route path="/">
+            <HomePage />
+          </Route>
+
+        </Switch>
+      </Router>
     </div>
   );
 }
