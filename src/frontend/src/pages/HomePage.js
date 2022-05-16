a6c2dca767ff666dab48f6f7392331df09252f6a
Lalit-N97 <lalitnegi1997@gmail.com>
2022-05-17 00:07:32 +0530

    fix server url at frontend

--- src/frontend/src/pages/HomePage.js
+++ src/frontend/src/pages/HomePage.js
@@ -6,25 +6,20 @@ import { TeamTile } from '../components/TeamTile';
 export const HomePage = () => {
 
     const [teams, setTeams] = useState([]);
-    useEffect(
-        () => {
-         const fetchAllTeams = async () => {
+    useEffect(() => {
+        const fetchAllTeams = async () => {
             const response = await fetch(`${process.env.REACT_APP_API_ROOT_URL}/team`);
             const data = await response.json();
             setTeams(data);
 
          };
          fetchAllTeams();
-            
-
-
-        }, []
-    );
+    }, []);
 
     return (
         <div className="HomePage">
             <div className="header-section">
-                <h1 className="app-name">Java Brains IPL Dashboard</h1>
+                <h1 className="app-name">IPL Dashboard</h1>
             </div>
             <div className="team-grid">
                 { teams.map(team => <TeamTile key={team.id} teamName={team.teamName} />)}
